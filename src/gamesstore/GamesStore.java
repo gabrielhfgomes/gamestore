19717dd02fd2b51e89deae8691c9e48280d65694
pedroguibueno <pedroguibueno@bol.com.br>
2015-11-18 20:25:15 -0200

    carrega

--- src/gamesstore/GamesStore.java
+++ src/gamesstore/GamesStore.java
@@ -5,6 +5,14 @@
  */
 package gamesstore;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author s2it_ggomes
@@ -14,8 +22,42 @@ public class GamesStore {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new JFrmMenu().setVisible(true);
+        GamesStore m1 = new GamesStore();
+        m1.leArquivo("Arquivo.txt");
+    }
+    
+     public void escreveArquivo(String nomeArquivo, boolean b1, int cont) throws IOException {
+
+        BufferedWriter buffWrite = new BufferedWriter(new FileWriter(nomeArquivo, b1));
+        buffWrite.append(String.valueOf(cont));  //no outro pacote
+        buffWrite.close();
+
+        System.out.println("Escrito o cont " + cont); //TESTE
+    }
+
+    public void leArquivo(String nomeArquivo) throws FileNotFoundException, IOException {
+        File arquivo = new File(nomeArquivo);
+
+        if (!arquivo.exists()) {
+            escreveArquivo(nomeArquivo, false, 1);
+        } else {
+
+            String linha;
+
+            FileReader fr = new FileReader(nomeArquivo);
+            BufferedReader br = new BufferedReader(fr);
+            linha = br.readLine();
+            int cont = Integer.parseInt(linha);
+            cont++;
+            
+
+            escreveArquivo(nomeArquivo, false, cont);
+            br.close();
+            fr.close();
+        }
+
     }
     
 }
